4335f1d7803935ffbfe4ae50bf5b04b8fd7f1ae2
ArThuca <arthurteixeiradeoliveira1234@gmail.com>
2026-04-09 12:18:24 -0300

    Add files via upload

---
+++ questão1.ts
@@ -0,0 +1,5 @@
+let idade: number
+let anoNasc: number = 1990
+let anoAtual: number = 2026
+idade = anoAtual - anoNasc                    
+console.log(`Sua idade em 2026 é ${idade}`)
---
+++ questão2.ts
@@ -0,0 +1,3 @@
+let reais:number = 100
+reais = reais / 5
+console.log(`Dá para comprar ${reais} dólars`)
